0de163c42f5bd634f1d0698e002277d184ee2a83
Henry <mail@henrygressmann.de>
2021-10-21 12:50:14 +0200

    add landing page
    Signed-off-by: Henry <mail@henrygressmann.de>

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -5,11 +5,17 @@ import styles from './index.module.scss';
 
 import logo from './pog.svg';
 
-export const Layout: FC = ({ children }) => (
+export const Layout: FC<{ className: string }> = ({ children, className }) => (
 	<div className={styles.layout}>
 		<div className={styles.sidebar}>
 			<img className={styles.logo} src={logo} />
 		</div>
-		<div className={styles.main}>{children}</div>
+		<div className={styles.main}>
+			<div className={styles.header}>
+				<img className={styles.logo} src={logo} />
+				<h1>pog.network wallet</h1>
+			</div>
+			<div className={`${styles.children} ${className}`}>{children}</div>
+		</div>
 	</div>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import { Layout } from './../components/layout';
+import styles from './index.module.scss';
 
 export const Index = () => (
-	<Layout>
-		<h1>MyPOG</h1>
+	<Layout className={styles.layout}>
+		<div className={styles.content}>
+			<h1>Welcome to MyPOG</h1>
+			<p>
+				MyPOG is the standard interface for interacting with the POG Network.
+				This client-side app allows you to open and generate wallets and much
+				more!
+			</p>
+			<div>
+				<button type="button" className={`${styles.button} ${styles.active}`}>
+					Create a new wallet
+				</button>
+				<button type="button" className={styles.button}>
+					Access my wallet
+				</button>
+			</div>
+		</div>
+		<div className={styles.hero}>
+			<img src="https://i.imgur.com/holg3bP.png" />
+		</div>
 	</Layout>
 );
