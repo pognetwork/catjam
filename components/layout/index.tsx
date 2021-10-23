074c524d10124ceec0fd46399386f21146a15a2c
Henry <mail@henrygressmann.de>
2021-10-23 12:52:07 +0200

    feat: add sidebar
    Signed-off-by: Henry <mail@henrygressmann.de>

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,33 +1,193 @@
 import React, { FC } from 'react';
-import { Link } from '@snowstorm/core';
+import { Link, useLocation } from '@snowstorm/core';
 
 import './global.scss';
 import logo from './pog.svg';
 import styles from './index.module.scss';
 
-export const Layout: FC<{ className: string }> = ({ children, className }) => (
-	<div className={styles.layout}>
-		<div className={styles.sidebar}>
-			<Link href="/">
-				<img height="30" width="30" className={styles.logo} src={logo} />
-			</Link>
-		</div>
-		<div className={styles.main}>
-			<div className={styles.header}>
-				<img height="30" width="30" className={styles.logo} src={logo} />
-				<h1>pog.network wallet</h1>
-			</div>
-			<div className={`${styles.children} ${className}`}>{children}</div>
-		</div>
-		<div className={styles.footer}>
-			<a
-				href="https://pog.network/impressum/"
-				target="_blank"
-				referrerPolicy="no-referrer"
-				rel="noreferrer"
-			>
-				legal notice/privacy policy
-			</a>
-		</div>
-	</div>
+const logoutIcon = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		className={styles.sidebarIcon}
+	>
+		<path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+		<polyline points="16 17 21 12 16 7" />
+		<line x1="21" y1="12" x2="9" y2="12" />
+	</svg>
 );
+
+const messageIcon = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		className={styles.sidebarIcon}
+	>
+		<path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
+	</svg>
+);
+
+const sendIcon = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		className={styles.sidebarIcon}
+	>
+		<line x1="22" y1="2" x2="11" y2="13" />
+		<polygon points="22 2 15 22 11 13 2 9 22 2" />
+	</svg>
+);
+
+const settingsIcon = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		className={styles.sidebarIcon}
+	>
+		<circle cx="12" cy="12" r="3" />
+		<path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
+	</svg>
+);
+
+const globeIcon = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		className={styles.sidebarIcon}
+	>
+		<circle cx="12" cy="12" r="10" />
+		<line x1="2" y1="12" x2="22" y2="12" />
+		<path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
+	</svg>
+);
+
+const codeIcon = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		className={styles.sidebarIcon}
+	>
+		<polyline points="16 18 22 12 16 6" />
+		<polyline points="8 6 2 12 8 18" />
+	</svg>
+);
+
+const sidebarLinks = [
+	{
+		name: 'Dashboard',
+		url: '/dashboard',
+		icon: globeIcon,
+	},
+	{
+		name: 'Transaction',
+		url: '/dashboard/send',
+		icon: sendIcon,
+	},
+	{
+		name: 'Messages',
+		url: '/dashboard/messages',
+		icon: messageIcon,
+	},
+	{
+		name: 'Contracts',
+		url: '/dashboard/aacs',
+		icon: codeIcon,
+	},
+];
+
+export const Layout: FC<{ className: string }> = ({ children, className }) => {
+	const [loc] = useLocation();
+
+	return (
+		<div className={styles.layout}>
+			<div className={styles.sidebar}>
+				<Link href="/">
+					<img height="30" width="30" className={styles.logo} src={logo} />
+				</Link>
+				<div className={styles.sidebarTop}>
+					{sidebarLinks.map(sidebar => (
+						<Link
+							key={sidebar.name}
+							className={`${styles.sidebarLink} ${
+								loc.endsWith(sidebar.url) && styles.active
+							}`}
+							href={sidebar.url}
+						>
+							{sidebar.icon}
+							<p>{sidebar.name}</p>
+						</Link>
+					))}
+					<div className={styles.sidebarHr} />
+					<Link
+						className={`${styles.sidebarLink} ${
+							loc.endsWith('settings') && styles.active
+						}`}
+						href="/settings"
+					>
+						{settingsIcon}
+						<p>Settings</p>
+					</Link>
+				</div>
+			</div>
+			<div className={styles.main}>
+				<div className={styles.header}>
+					<img height="30" width="30" className={styles.logo} src={logo} />
+					<h1>pog.network wallet</h1>
+				</div>
+				<div className={`${styles.children} ${className}`}>{children}</div>
+			</div>
+			<div className={styles.footer}>
+				<a
+					href="https://pog.network/impressum/"
+					target="_blank"
+					referrerPolicy="no-referrer"
+					rel="noreferrer"
+				>
+					legal notice/privacy policy
+				</a>
+			</div>
+		</div>
+	);
+};
---
+++ pages/dashboard.tsx
@@ -0,0 +1,6 @@
+import { Layout } from './../components/layout';
+import styles from './dashboard.module.scss';
+
+export const Dashboard = () => (
+	<Layout className={styles.layout}>Dashboard</Layout>
+);
